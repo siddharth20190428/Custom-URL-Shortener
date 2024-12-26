df8a830b0ff706214c84c2f16db2aa7bb3e5544f
Siddharth Sahu <siddharth201820@gmail.com>
2024-12-26 16:51:32 +0530

    added deploy commands

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: "localhost",
+  host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
